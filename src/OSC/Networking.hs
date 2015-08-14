5d748fd505a9f97a270cc52bc97b353bf1966082
Cole Ingraham <coledingraham@gmail.com>
2015-08-14 20:34:35 +0800

    fixed osc message passing from haskell to lua, added basic error messages for sytax errors

--- src/OSC/Networking.hs
+++ src/OSC/Networking.hs
@@ -2,6 +2,7 @@ module OSC.Networking where
 
 import           Control.Concurrent
 import           Control.Monad
+import qualified Data.ByteString.Char8 as BC
 import           Sound.OSC
 import qualified Scripting.Lua as Lua
 import           Util.State
@@ -39,19 +40,36 @@ receiveFunc :: HydraState -> Message -> IO ()
 receiveFunc h m
     | addr == "/code" = do
         Lua.loadstring ls (ascii_to_string $ d_ascii_string $ head msg) ""
-        Lua.pcall ls 0 0 1
-        return ()
+        result <- Lua.pcall ls 0 0 1
+        checkResult ls result
     | otherwise       = do
-        Lua.loadstring ls ("receiveOscMessage(" ++ (show addr) ++ ",{" ++ arglist ++ "})") ""
-        Lua.pcall ls 0 0 1
+        Lua.getglobal ls "receiveOscMessage"
+        Lua.pushstring ls $ BC.pack addr
+        args ls
+        Lua.pcall ls (numVals + 1) 0 0
         return ()
     where
+        checkResult l r
+            | r == 0 = return ()
+            | otherwise = do
+                e <- Lua.tostring l (-1)
+                print $ "Error: " ++ (show e)
+                return ()
         addr      = messageAddress m
         msg       = messageDatum m
         ls        = lua_state $ nodes h
-        args      = map showVal msg
-        arglist   = foldl (\acc x -> acc ++ x ++ "," ) "" $ args
-        showVal v = case datum_tag v of
-                            's' -> show $ ascii_to_string $ d_ascii_string v
-                            'i' -> show $ d_int32 v
-                            'f' -> show $ d_float v
+        args l    = mapM_ (pushVal l) msg
+        numVals   = length msg
+        pushVal l v = case datum_tag v of
+                            's' -> do
+                                Lua.pushstring l $ BC.pack $ ascii_to_string $ d_ascii_string v
+                                return ()
+                            'i' -> do
+                                Lua.pushnumber l $ realToFrac $ d_int32 v
+                                return ()
+                            'f' -> do
+                                Lua.pushnumber l $ realToFrac $ d_float v
+                                return ()
+                            'd' -> do
+                                Lua.pushnumber l $ d_double v
+                                return ()
